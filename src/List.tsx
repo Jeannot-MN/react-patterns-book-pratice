afbc4b90bbbf14cf0493e785fb59b80acbc948ee
JeannotMN <jeannot@ssquared.dev>
2022-10-19 10:39:03 +0200

    Naive approach, no memo, no useMemo nor useCallback

---
+++ src/List.tsx
@@ -0,0 +1,28 @@
+import { FC, useEffect } from 'react';
+import Task from './Task';
+
+type Todo = {
+    id: number
+    task: string
+}
+
+interface Props {
+    todoList: Todo[]
+}
+
+const List: FC<Props> = ({ todoList }) => {
+
+    useEffect(() => {
+        console.log("List rendering...");
+    });
+
+    return (
+        <ul>
+            {todoList.map(todo => (
+                <Task key={todo.id} {...todo} />
+            ))}
+        </ul>
+    )
+}
+
+export default List;
